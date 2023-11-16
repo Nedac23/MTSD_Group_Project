fa42416918f1a56483fb8391f64450ee9cb28ad0
Caden Thompson <Cadenjacobthompson2@gmail.com>
2023-11-15 23:01:32 -0600

    Added Functionality To the decreaseStock function
    Completed the basic functionality of the decreaseStock function. Currently it only works on the database I have on my local machine.

--- Inventory.py
+++ Inventory.py
@@ -1,5 +1,6 @@
 from inspect import _void
 import sqlite3
+import string
 import sys
 class Inventory:
     #zero consturctor
@@ -26,6 +27,7 @@ class Inventory:
         return self.tableName
     #view inventory function
     def viewInventory(self):
+            #testing connection to database
             try:
                 connection = sqlite3.connect("Inventory.db")
 
@@ -40,13 +42,15 @@ class Inventory:
             print()
             ## cursor to send queries through
             cursor = connection.cursor()
+            #executing SQL Query
             cursor.execute("SELECT * FROM books")
 
-
+            #Gets the results of the query
             result = cursor.fetchall()
+            #prints the results
             for x in result:
    
-                print("ISBN:", x[0],"Title:", x[1], "\tAuthor:", x[2] ) ## only the ISBN
+                print("ISBN:", x[0]," Title:", x[1], " Author:", x[2], " Stock", x[5] ) 
                 print("\n")
 
 
@@ -57,10 +61,71 @@ class Inventory:
     
     #search Inventory function
     def searchInventory(self):
-        return 0
-    #decrease stock function
-    def decreaseStock(self, ISBN):
+        try:
+            connection = sqlite3.connect("Inventory.db")
+
+            print("Successful connection.")
+
+        except:
+            print("Failed connection.")
+
+        ## exits the program if unsuccessful
+            sys.exit()
+            
+        print()
+        ## cursor to send queries through
+        cursor = connection.cursor()
+        cursor.close()
+        connection.close()
+          
         return 0
 
+    #decrease stock function
+    def decreaseStock(self, ISBN):
+        #testing connection
+        try:
+                connection = sqlite3.connect("Inventory.db")
+
+                print("Successful connection.")
+
+        except:
+                print("Failed connection.")
+
+            ## exits the program if unsuccessful
+                sys.exit()
+                
+        print()
+        ## cursor to send queries through
+        cursor = connection.cursor()
+
+        #selects the stock value using the ISBN since it is unique
+        query = "SELECT * FROM books WHERE ISBN =? "
+        data = (ISBN,)
+        cursor.execute(query, data)
+
+        #results from the select query
+        result = cursor.fetchall()
+       #sets the result to the varibale stock_count
+        for x in result:
+           stock_count = (x[5])
+           #print(stock_count)
+        
+       #decreases the stock
+        stock_count = stock_count - 1
+       # print(stock_count)
+
+       #updates the database with the new stock value
+        query = "UPDATE books SET Num =? Where ISBN =? "
+        data = (stock_count, ISBN,)
+        cursor.execute(query,data)
+
+        #commits changes
+        connection.commit()
+        #closes cursor and connection
+        cursor.close()
+        connection.close()
+        return 0
+        
+       
 
         
