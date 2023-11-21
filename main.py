adfcf7f1b43a0c7503a2ab33c5e7f44c25c5e0eb
Caden Thompson <Cadenjacobthompson2@gmail.com>
2023-11-20 18:34:07 -0600

    Added Basic Menu for Inventory
    Added menu for inventory after the user logs in. Still needs work.

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import Inventory
 
+#class declaration
 I = Inventory.Inventory("Inventory.db","books")
-I.viewInventory()
-I.searchInventory()
+
+
+
+#After Login
+option = input("Please select a menu option:\n(0) View Inventory\n(1) Search Inventory\n(2) Go Back:  ")
+while(option != "2"):
+
+    #option = input("Please select a menu option:\n (0) View Inventory\n(1) Search Inventory:  ")
+    if(option == "0"):
+        I.viewInventory()
+    elif(option == "1"):
+        I.searchInventory()
+    option = input("Please select a menu option:\n(0) View Inventory\n(1) Search Inventory\n(2) Go Back:  ")
 
 
 
