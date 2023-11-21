3713f63e4df8d9babd043bf21703740a4ff5ef96
Keifer-H <149847787+Keifer-H@users.noreply.github.com>
2023-11-20 19:15:50 -0600

    Update to main.py (Added the Before Login Menu)

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
 import Inventory
+import User
+import Cart
 
 #class declaration
 I = Inventory.Inventory("Inventory.db","books")
 
+#Before Login
+user = User("User.db", "Users")
 
+while(True):
+    print("""1. Log In to an Existing Account
+         2. Create an Account
+         3. Close the Site
+         Which option would you like to select?\n""")
+
+    user_input = input("")
+            
+    if(user_input == "3"):
+        break
+        
+    elif(user_input == "1"):
+        user.login()
+            
+    elif(user_input == "2"):
+        user.createAccount()
+            
+    else:
+        print("Invalid menu option. Please try again.")
+            
 
 #After Login
 option = input("Please select a menu option:\n(0) View Inventory\n(1) Search Inventory\n(2) Go Back:  ")
