062fe5fb2dc477937e11e511de833fc2ad821e55
Caden Thompson <Cadenjacobthompson2@gmail.com>
2023-11-29 12:48:47 -0600

    Updated Main
    Added The After Login Menu to Main. It does not include the View User Information or the Logout Option at the moment.

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ import Cart  #also looking at her python example file she imported it as ->>    
 I = Inventory.Inventory("Inventory.db","Inventory")    #looking at her python example file i don't think we need to do Class.Class
 C = Cart("Cart.db","Cart")
 user = User("User.db", "Users")
+
+closed = False
 #Before Login
-
-
 while(True):
     print("""1. Log In to an Existing Account
          2. Create an Account
@@ -18,6 +18,8 @@ while(True):
     user_input = input("")
             
     if(user_input == "3"):
+        #sets closed equal to true to ensure the user doesnt go to the after login menu
+        closed = True
         break
         
     elif(user_input == "1"):
@@ -28,31 +30,53 @@ while(True):
             
     else:
         print("Invalid menu option. Please try again.")
-            
 
-#After Login
-option = input("Please select a menu option:\n(0) View Inventory\n(1) Search Inventory\n(2) Go Back:  ")
-while(option != "2"):
+#After Login Menu
 
-    #option = input("Please select a menu option:\n (0) View Inventory\n(1) Search Inventory:  ")
-    if(option == "0"):
-        I.viewInventory()
-    elif(option == "1"):
-        I.searchInventory()
-    option = input("Please select a menu option:\n(0) View Inventory\n(1) Search Inventory\n(2) Go Back:  ")
+#if statement to make sure user is loged in or made an account
+if(closed ==False):
+    while(True):
+        print("""1. Logout
+            2. View Account Information
+            3. Inventory Information
+            4. Cart Information
+            \n""")
+        #gets user input for menuing
+        menu_option = input("Please select a menu option:")
 
-#AFTER LOGIN -- CART INFO
-cartoptions = input("Please select a menu option:\n(0) View Cart\n(1) Add Items to Cart\n(2) Remove an Item from Cart\n(3) Check Out\n(4) Go Back: ")
-while(cartoptions != 4):
-    if(option == 0):
-        C.viewcart(user.getUserID,I.getdatabaseName)
-    elif(option == 1):
-        ad = input("Please type ISBN of book: ")
-        C.additem(user.getUserID, ad)
-    elif(option ==2):
-        re = input("Please type ISBN of book: ")
-        C.additem(user.getUserID, re)
-        C.removeitem
-    elif(option == 3):
-        C.checkout(user.getUserID)
+        #logout option
+            #break
+        #view account information
+
+        #menu option for inventory information
+        if(menu_option == 3):
+            #After Login
+            option = input("Please select a menu option:\n(0) View Inventory\n(1) Search Inventory\n(2) Go Back:  ")
+            while(option != "2"):
+
+                #option = input("Please select a menu option:\n (0) View Inventory\n(1) Search Inventory:  ")
+                if(option == "0"):
+                    I.viewInventory()
+                elif(option == "1"):
+                    I.searchInventory()
+                option = input("Please select a menu option:\n(0) View Inventory\n(1) Search Inventory\n(2) Go Back:  ")
+
+        if(menu_option == 4):
+            #AFTER LOGIN -- CART INFO
+
+            ####ATTENTION
+            #need to change your option variable to cartoptions
+            cartoptions = input("Please select a menu option:\n(0) View Cart\n(1) Add Items to Cart\n(2) Remove an Item from Cart\n(3) Check Out\n(4) Go Back: ")
+            while(cartoptions != 4):
+                if(option == 0):
+                    C.viewcart(user.getUserID,I.getdatabaseName)
+                elif(option == 1):
+                    ad = input("Please type ISBN of book: ")
+                    C.additem(user.getUserID, ad)
+                elif(option ==2):
+                    re = input("Please type ISBN of book: ")
+                    C.additem(user.getUserID, re)
+                    C.removeitem
+                elif(option == 3):
+                    C.checkout(user.getUserID)
 
